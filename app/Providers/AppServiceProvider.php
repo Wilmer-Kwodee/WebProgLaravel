e324eeae43f567619b5717c95d7d355c492311a6
Kwodee <wilmerkwodeevan@gmail.com>
2024-12-16 23:30:56 +0700

    revert, AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
+            // URL::forceScheme('https');
         }
     }
 }
