5434fd866cd9eb51d56941aa462bdebcd54fcb07
Wilmer Kwodee <152074108+Wilmer-Kwodee@users.noreply.github.com>
2024-12-17 17:31:52 +0700

    t8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+
 /** @type {import('tailwindcss').Config} */
-
-// const defaultTheme = require('tailwindcss/defaultTheme')
-import defaultTheme from 'tailwindcss/defaultTheme';
-
-
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-        fontFamily: {
-        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
-      },
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views//*.blade.php",
+        "./resources//*.blade.php",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [],
-}
+
+    plugins: [forms],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    build: {
+        outDir: "public/build",
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
