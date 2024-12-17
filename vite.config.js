b63c62021be22e3cf761ea50b09863921c51c909
RV <100607241+Sushrimp@users.noreply.github.com>
2024-12-17 20:46:25 +0700

    fix: slight changes local and vite dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: "public/build",
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
