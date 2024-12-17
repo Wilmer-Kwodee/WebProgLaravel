53c1c162995cc7980ca91efb915c95bc99e47d14
Kwodee <wilmerkwodeevan@gmail.com>
2024-12-17 12:18:09 +0700

    feat: tifis tifis 2

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -31,31 +31,54 @@
         <div class="mb-10">
             <p class="font-bold text-3xl">Challenges History:</p>
             <article class="py-3 my-6 w-[100%] bg-cyan-200 rounded-xl">
-            <input 
-                type="checkbox" 
-                class="ml-4 challenge-checkbox" 
+            <input
+                type="checkbox"
+                class="ml-4 challenge-checkbox"
             />
             <label class="ml-4 text-2xl font-bold italic">Turn off taps when not in use</label>
             </article>
         </div>
         <div class="mb-10">
             <p class="font-bold text-3xl">My community posts:</p>
-            {{-- <article class="my-6 max-w-sm rounded-lg overflow-hidden shadow-lg bg-white border border-gray-300">
-                <div class="flex items-center p-4">
-                    <img class="w-12 h-12 rounded-full border-2 border-blue-500"
-                        src="https://via.placeholder.com/150" />
-                    <div class="ml-4">
-                        <div class="flex">
-                            <p class="text-lg font-bold text-gray-800">username</p>
-                            <p class="text-sm text-gray-500 ml-5 font-bold">5 mins</p>
+
+            @if (count($myPosts) > 0)
+            <div class="flex">
+                @foreach ($myPosts as $post)
+                <a href="{{ url('/community/detail/' . $post['id']) }}">
+                    <div
+                        class="w-auto mx-auto mt-5 rounded-lg overflow-hidden shadow-lg border border-gray-300 bg-white">
+                        <div class="flex items-center justify-between px-4 py-3">
+                            <div class="flex items-center">
+                                <img class="w-10 h-10 rounded-full border border-gray-300"
+                                    src="https://via.placeholder.com/100" />
+                                <div class="ml-3">
+                                    <h3 class="text-sm font-bold text-gray-800">{{ $post['user']['name'] }}</h3>
+                                    <p class="text-xs text-gray-500">5 mins •</p>
+                                </div>
+                            </div>
+                            <button class="text-gray-500 hover:text-gray-700">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24"
+                                    fill="currentColor">
+                                    <path
+                                        d="M12 10.5a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm0 5.25a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm0 5.25a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+                                </svg>
+                            </button>
                         </div>
-                        <div>
-                            <p class="text-md text-gray-800">Title</p>
+                        <div class="px-4 py-2">
+                            <p class="text-sm text-gray-800">{{ $post['title'] }}</p>
                         </div>
+                        @if ($post['image'])
+                            <img class="w-full h-64 object-cover" src={{$post['image']}} alt="Image not found" />
+                        @endif
+                        @php
+                        @endphp
                     </div>
-                </div>
-                <img class="w-full h-36 object-cover" src="" />
-            </article> --}}
+                </a>
+                @endforeach
+            </div>
+            @else
+                <p>You haven't posted anything yet...</p>
+            @endif
         </div>
     </div>
     <x-footer/>
